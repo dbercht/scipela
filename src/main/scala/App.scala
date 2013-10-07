2ea1b5ddc3640f166e15b88bbbde37cf22d8e788
Dan <d.bercht@gmail.com>
2013-10-07 00:52:06 -0400

    Fixed building pipeline implementation

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -4,46 +4,37 @@ import simulator.entities._
 object Simulator {
 
   def makePipeline():Pipeline = {
-    val pipeline = Pipeline(List())
+    val emptyQ = Queue(0)
 
-    val fraudJenkinsQ = Queue(0)
-    val fraudJenkinsJob = JenkinsJob(fraudJenkinsQ, Config.jenkinsDelay, "fraud_jenkins")
-    val fraudStormQ = Queue(0)
-    val fraudStormWorker = Worker(Config.fraudProcessingTime, Config.fraudProcessingTime)
-    val fraudStormJob = StormJob(fraudStormQ, Seq(fraudStormWorker, fraudStormWorker), new StormDelay, "fraud_storm")
+    val fraudJenkinsJob = JenkinsJob(emptyQ, Config.jenkinsDelay, "fraud_jenkins")
+    val fraudStormWorker = Delay(Config.fraudProcessingTime, Config.fraudProcessingTime, false)
+    val fraudStormJob = StormJob(emptyQ, Seq(fraudStormWorker, fraudStormWorker), new StormDelay, "fraud_storm")
 
-    val asJenkinsQ = Queue(0)
-    val asJenkinsJob = JenkinsJob(asJenkinsQ, Config.jenkinsDelay, "as_jenkins")
-    val asStormQ = Queue(0)
-    val asStormWorker = Worker(Config.assignSupplierProcessingTime, Config.assignSupplierProcessingTime)
-    val asStormJob = StormJob(asStormQ, Seq(asStormWorker, asStormWorker), new StormDelay, "as_storm")
+    val asJenkinsJob = JenkinsJob(emptyQ, Config.jenkinsDelay, "as_jenkins")
+    val asStormWorker = Delay(Config.assignSupplierProcessingTime, Config.assignSupplierProcessingTime, false)
+    val asStormJob = StormJob(emptyQ, Seq(asStormWorker, asStormWorker), new StormDelay, "as_storm")
+    val lastJob = JenkinsJob(emptyQ, Config.jenkinsDelay, "last_job")
 
-    pipeline
-    		.addLink(MultipleLinks(fraudJenkinsJob, List((.3f -> asJenkinsJob), (.7f -> fraudStormJob))))
-    		.addLink(SingleLink(fraudStormJob, asJenkinsJob))
-    		.addLink(SingleLink(asJenkinsJob, asStormJob))
+    val links = List(
+      Link(fraudJenkinsJob, List((.3f -> asJenkinsJob), (.7f -> fraudStormJob))),
+      Link(fraudStormJob, List(1f -> asJenkinsJob)),
+      Link(asJenkinsJob, List(1f -> asStormJob)),
+      Link(asJenkinsJob, List(1f -> asStormJob)),
+      Link(asStormJob, List(1f -> lastJob))
+    )
+
+    Pipeline.buildFromLinks(links)
   }
 
-  def tick(pipeline: Pipeline) {
-    println(pipeline.links.length)
-    for (link <- pipeline.links) yield {
-      link match {
-        case MultipleLinks(job: JenkinsJob, next: List[(Float, Job)]) => println(job.toString)
-        case MultipleLinks(job: StormJob, next: List[(Float, Job)]) => println(job.toString)
-        case SingleLink(job: Job, nextJob: Job) => println(job.toString)
-      }
-    }
-  }
-  
+
   def print(pipeline: Pipeline) {
-    val pipeStr = for(link <- pipeline.links) yield {
-      println(link.linkMap.toString)
-    }
+    println(pipeline.jobMap)
   }
 
   def main(args: Array[String]) {
     val pipeline = makePipeline()
-    val pipeline2 = print(pipeline)
-
+    print(pipeline)
+//    println(pipeline)
+    println(pipeline.tock)
   }
 }
